6fb70a224d006dbbfcb27fc99dbc614303af5635
pujasodha <34947997+pujasodha@users.noreply.github.com>
2018-04-21 00:06:04 -0700

    need to fix bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,39 @@
-var friendData = require("../data/peopleList.js");
+var friends = require("../data/peopleList.js");
 
 var path = require('path');
 
 
-module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.json(friendData);
-    });
+module.exports = function(app) {
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
+	app.post('/api/friends', function(req, res) {
+		var userInput = req.body;
 
-    app.post("/api/friends", function (req, res) {
-        var userInput = req.body;
-        var userResponse = userInput.scores;
-        var matchName = '';
-        var matchImage = '';
-        var totalDifference = 10000;
+		var userResponses = userInput.scores;
 
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000; 
 
-        for (var i = 0; i < peopleInfo.length; i++) {
-            var diff = 0;
-            for (var j = 0; j < userResponse.length; j++) {
-                diff += Math.abs(peopleInfo[i].scores[j] - userResponse[j]);
-            }
-            if (diff < totalDifference) {
-                totalDifference = diff;
-                matchName = peopleInfo[i].name;
-                matchImage = peopleInfo[i].photo;
-            }
-        }
-        res.json({
-            status: 'OK',
-            matchName: matchName,
-            matchImage: matchImage
-        });
+		for (var i = 0; i < friends.length; i++) {
+			
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			
+			if (diff < totalDifference) {
+				
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
 
-    })
-}
+		friends.push(userInput);
+
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
